b45cc69c4552372635d009b0d95ee377742fe6fa
Jean Dias <diasjeanferreira@gmail.com>
2026-09-18 09:44:54 -0300

    feat(ws): gerente de conexões com salas

---
+++ app/ws.py
@@ -0,0 +1,22 @@
+from fastapi import WebSocket
+
+class Gerente:
+    def __init__(self):
+        self.salas: dict[str, set[WebSocket]] = {"telao": set(), "mesario": set()}
+
+    def adicionar(self, ws: WebSocket, sala: str):
+        self.salas[sala].add(ws)
+
+    def remover(self, ws: WebSocket):
+        for conexoes in self.salas.values():
+            conexoes.discard(ws)
+
+    async def broeadcast(self, msg: dict, *salas: str):
+        for sala in salas:
+            for ws in list(self.salas[sala]):
+                try:
+                    await ws.send_json(msg)
+                except Exception:
+                    self.remover(ws)
+
+gerente = Gerente()
